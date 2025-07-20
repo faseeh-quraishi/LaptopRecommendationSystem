82e31835d44110185173f80c789c434ad02f4b26
faseeh-quraishi <faseeh.quraishi.fq@gmail.com>
2025-07-20 15:47:17 -0400

    Faseeh - JsonParsr import issue resolved

--- src/laptoprecommendation/WebInterface.java
+++ src/laptoprecommendation/WebInterface.java
@@ -18,6 +18,8 @@ import java.util.Map.Entry;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonArray;
 import com.google.gson.Gson;
+import com.google.gson.JsonParser;
+
 
 public class WebInterface {
 
